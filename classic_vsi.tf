a4588ba1b17ed85558a2a58280ebc7fc832e248e
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-24 16:12:58 +0900

    Create classic_vsi.tf

---
+++ classic_vsi.tf
@@ -0,0 +1,22 @@
+#プロバイダーをSoftLayerに設定
+provider "softlayer" {
+    username = "xxxxxxxxxxx"
+    api_key = "xxxxxxxxxxx"
+}
+
+#リソースの設定
+# Virtual Server created with existing SSH Key already in SoftLayer
+resource "softlayer_virtual_guest" "khayama01" {
+    name = "khayama01"
+    domain = "softlayer.com"
+    ssh_keys = ["776719"]
+    image = "CENTOS_7_64"
+    region = "tok02"
+    public_network_speed = 100
+    cpu = 1
+    ram = 1024
+    hourly_billing = true
+    private_network_only = false
+    disks = [25]
+    local_disk = true
+}
