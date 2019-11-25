3439a4e535920c65c12fc017f404a5c7378d8d53
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-25 19:01:56 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,12 @@
-variable "ibm_sl_username" {}
-variable "ibm_sl_api_key" {}
+variable "iaas_classic_username" {
+  description = "Enter the user name to access IBM Cloud classic infrastructure. You can retrieve the user name by following the instructions for retrieving your classic infrastructure API key."
+}
+
+variable "iaas_classic_api_key" {
+  description = "Enter the API key to access IBM Cloud classic infrastructure. For more information for how to create an API key and retrieve it, see [Managing classic infrastructure API keys](https://cloud.ibm.com/docs/iam?topic=iam-classic_keys)."
+}
 
 provider "ibm" {
-  iaas_classic_username = "${var.ibm_sl_username}"
-  iaas_classic_api_key  = "${var.ibm_sl_api_key}"
+  iaas_classic_username = "${var.iaas_classic_username}"
+  iaas_classic_api_key  = "${var.iaas_classic_api_key}"
 }
