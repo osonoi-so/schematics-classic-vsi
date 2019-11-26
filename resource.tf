d66d7c9f48740328241e4c15cc576130212d400a
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-26 18:04:42 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,14 +1,29 @@
+variable "datacenter" {
+  type        = "string"
+  default = "tok04"
+}
+
+variable "os_reference_code" {
+  type        = "string"
+  default = "CENTOS_7_64"
+}
+
+variable "flavor_key_name" {
+  type        = "string"
+  default = "B1_1X1X25"
+}
+
 resource "ibm_compute_vm_instance" "khayama-test" {
     hostname = "khayama-test"
     domain = "ibmcloud.com"
-    os_reference_code = "CENTOS_7_64"
-    datacenter = "tok02"
+    os_reference_code = "${var.os_reference_code}"
+    datacenter = "${var.datacenter}"
     network_speed = 100
     hourly_billing = true
     transient = true
     local_disk = false
     private_network_only = false
-    flavor_key_name = "B1_2X4X25"
+    flavor_key_name = "${var.flavor_key_name}"
     public_security_group_ids = 1287613
     tags = ["user:khayama"]
     notes = "khayama-test"
