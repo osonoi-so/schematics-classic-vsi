13a438eeb211382501356048b0357885a46c3d47
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-26 17:50:32 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,14 +1,13 @@
-resource "ibm_compute_vm_instance" "khayama01" {
-    name = "khayama01"
+resource "ibm_compute_vm_instance" "khayama-test" {
+    hostname = "khayama-test"
     domain = "ibmcloud.com"
-    ssh_keys = ["xxxxx"]
-    image = "CENTOS_7_64"
-    region = "tok02"
-    public_network_speed = 100
-    cpu = 1
-    ram = 1024
+    os_reference_code = "CENTOS_7_64"
+    datacenter = "tok02"
+    network_speed = 100
     hourly_billing = true
+    local_disk = false
     private_network_only = false
-    disks = [25]
-    local_disk = true
+    flavor_key_name = "B1_2X8X25"
+    tags = ["user:khayama"]
+    notes = "khayama-test"
 }
